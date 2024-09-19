bf09d7ccdd5d5a0737b77eb36bc3f8f6da3ebb5f
John Binzak <john.binzak@gmail.com>
2024-09-19 07:18:13 -0500

    Adding initial jobba component and PNG logo

--- AngularFrontend/src/app/app.component.ts
+++ AngularFrontend/src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.png" alt="logo" aria-hidden="true" height="50px">
+    </header>
+    <section class="content">
+      <app-jobba></app-jobba>
+    </section>
+  </main>
+`,
 })
 export class AppComponent {
   title = 'AngularFrontend';
---
+++ AngularFrontend/src/app/jobba/jobba.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobba',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      jobba works!
+    </p>
+  `,
+  styleUrls: ['./jobba.component.scss']
+})
+export class JobbaComponent {
+
+}
